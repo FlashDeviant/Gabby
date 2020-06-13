505dfdd163d8d248c33b39149b75c5dc78380181
Pentagrade <pentagrade@outlook.com>
2020-06-13 17:34:21 +0100

    Migrate CommandHandler

--- Gabby/Gabby/Handlers/CommandHandler.cs
+++ Gabby/Gabby/Handlers/CommandHandler.cs
@@ -2,60 +2,59 @@
 {
     using System;
     using System.Threading.Tasks;
-    using Discord;
-    using Discord.Commands;
-    using Discord.WebSocket;
-    using Gabby.Modules;
+    using DSharpPlus;
+    using DSharpPlus.CommandsNext;
+    using DSharpPlus.Entities;
+    using DSharpPlus.EventArgs;
     using JetBrains.Annotations;
     using Microsoft.Extensions.Configuration;
 
     [UsedImplicitly]
     public sealed class CommandHandler
     {
-        private readonly CommandService _commands;
+        private readonly CommandsNextExtension _commands;
         private readonly IConfigurationRoot _config;
-        private readonly DiscordSocketClient _discord;
-        private readonly IServiceProvider _provider;
+        private readonly DiscordClient _discord;
 
         // DiscordSocketClient, CommandService, IConfigurationRoot, and IServiceProvider are injected automatically from the IServiceProvider
         public CommandHandler(
-            DiscordSocketClient discord,
-            CommandService commands,
+            DiscordClient discord,
+            CommandsNextExtension commands,
             IConfigurationRoot config,
             IServiceProvider provider)
         {
             this._discord = discord;
             this._commands = commands;
             this._config = config;
-            this._provider = provider;
 
-            this._discord.MessageReceived += this.OnMessageReceivedAsync;
+            this._discord.MessageCreated += this.OnMessageCreatedAsync;
+            this._commands.CommandErrored += this.OnCommandErrored;
+            this._commands.CommandExecuted += this.OnCommandExecuted;
         }
 
-        private async Task OnMessageReceivedAsync(SocketMessage s)
+        private async Task OnCommandExecuted([NotNull] CommandExecutionEventArgs e)
         {
-            if (!(s is SocketUserMessage msg)) return;
+            await e.Context.Message.DeleteAsync();
+        }
+
+        private async Task OnCommandErrored([NotNull] CommandErrorEventArgs e)
+        {
+            var embed = EmbedHandler.GenerateEmbedResponse("Uh oh, something went wrong:\r\n" + $"{e.Exception.Message}\r\n\r\nPlease try using '{this._config["Prefix"]}help <command>' to find out how to use this command",
+                DiscordColor.Red);
+            await e.Context.Channel.SendMessageAsync("", false, embed).ConfigureAwait(false);
+        }
+
+        private async Task OnMessageCreatedAsync([NotNull] MessageCreateEventArgs msg)
+        {
             if (msg.Author.Id == this._discord.CurrentUser.Id) return; // Ignore self when checking commands
 
-            var context = new SocketCommandContext(this._discord, msg); // Create the command context
-
-            var argPos = 0; // Check if the message has a valid command prefix
-            if (msg.HasStringPrefix(this._config["Prefix"], ref argPos) ||
-                msg.HasMentionPrefix(this._discord.CurrentUser, ref argPos))
+            if (msg.Message.GetStringPrefixLength(this._config["Prefix"]) == 0 ||
+                msg.Message.GetMentionPrefixLength(this._discord.CurrentUser) == 0)
             {
-                var result = await this._commands.ExecuteAsync(context, argPos, this._provider)
+                var context = this._commands.CreateContext(msg.Message, this._config["Prefix"],
+                    this._commands.FindCommand(msg.Message.ToString(), out var args), args); // Create the command context
+                await this._commands.ExecuteCommandAsync(context)
                     .ConfigureAwait(false); // Execute the command
-
-                if (!result.IsSuccess) // If not successful, reply with the error.
-                {
-                    var embed = EmbedHandler.GenerateEmbedResponse("Uh oh, something went wrong:\r\n" + $"{result}\r\n\r\nPlease try using '{this._config["Prefix"]}help <command>' to find out how to use this command",
-                        Color.Red);
-                    await context.Channel.SendMessageAsync("", false, embed).ConfigureAwait(false);
-                }
-                else
-                {
-                    await msg.DeleteAsync();
-                }
             }
         }
     }
