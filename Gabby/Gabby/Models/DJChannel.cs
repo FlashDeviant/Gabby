0ef670ba8c7bfffc40530fab198d998166d729c7
Pentagrade <pentagrade@outlook.com>
2020-06-14 20:19:39 +0100

    Clean models

---
+++ Gabby/Gabby/Models/DJChannel.cs
@@ -0,0 +1,12 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace Gabby.Models
+{
+    [DynamoDBTable("DJChannel")]
+    public class DJChannel
+    {
+        [DynamoDBHashKey] public string GuildGuid { get; set; } //ulong
+
+        public string ChannelId { get; set; }
+    }
+}
